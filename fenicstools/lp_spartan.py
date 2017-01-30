6fe7d7f1e257518526ee0f5d6d6eb91f218def06
miro3 <miroslav.kuchta@gmail.com>
2017-01-30 16:03:11 +0100

    things move in parallel. now see if they move the right way

--- fenicstools/lp_spartan.py
+++ fenicstools/lp_spartan.py
@@ -1,12 +1,8 @@
+from collections import defaultdict
+from itertools import count
 import dolfin as df
 import numpy as np
 
-
-def counter():
-    c = 0
-    while True: 
-        yield c
-        c += 1
 
 class CellWithParticles(df.Cell):
     '''TODO'''
@@ -38,6 +34,7 @@ class LPCollection(object):
         mesh = V.mesh()
         self.dim = mesh.geometry().dim()
         assert self.dim == mesh.topology().dim()
+        mesh.init(0, self.dim)
         self.mesh = mesh
 
         self.tree = mesh.bounding_box_tree()
@@ -53,7 +50,7 @@ class LPCollection(object):
 
         self.particles = {}
         self.cells = {}
-        self.ticket = counter()
+        self.ticket = count()
 
         comm = mesh.mpi_comm().tompi4py()
         assert comm.size == 1 or comm.size % 2 == 0
@@ -61,12 +58,24 @@ class LPCollection(object):
         self.prev_rank = (comm.rank + comm.size - 1) % comm.size
         self.comm = comm
 
+        element = V.dolfin_element()
+        
+        num_tensor_entries = 1
+        for i in range(element.value_rank()): num_tensor_entries *= element.value_dimension(i)
+
+        self.coefficients = np.zeros(element.space_dimension())
+        self.basis_matrix = np.zeros((element.space_dimension(), num_tensor_entries))
+
+        self.element = element
+        self.num_tensor_entries = num_tensor_entries
+
+
     def locate(self, x):
         point = df.Point(*x)
         c = self.tree.compute_first_entity_collision(point)
         return c if c < self.lim else -1
 
-    def add_particles(self, particles):
+    def __add_particles_local(self, particles):
         not_found = []
         for p in particles:
             x = p[:self.dim]
@@ -75,17 +84,23 @@ class LPCollection(object):
             if c > -1: 
                 key = next(self.ticket)
                 self.particles[key] = p
-                if c in self.cells:
-                    self.cells[c] + key
-                else:
+                if c not in self.cells:
                     self.cells[c] = CellWithParticles(self, c, key)
+                self.cells[c] + key
                 key += 1
             else:
                 not_found.append(p)
+        return not_found
+
+    def add_particles(self, particles):
+        not_found = self.__add_particles_local(particles)
         count = len(not_found)
         count_global = self.comm.allgather(count)
-        # if self.comm.rank == 0: info('>> %s' % count_global)
 
+        self.__add_particles_global(count_global, not_found)
+
+    
+    def __add_particles_global(self, count_global, not_found):
         loop = 1
         while max(count_global) > 0 and loop < self.comm.size:
             loop += 1
@@ -102,64 +117,96 @@ class LPCollection(object):
                 self.comm.Send(np.array(not_found).flatten(), self.next_rank, self.comm.rank)
             # Work with received
             received = received.reshape((-1, self.psize))
-            not_found = []
-            for p in received:
-                x = p[:self.dim]
-
-                c = self.locate(x)
-                if c > -1: 
-                    key = next(self.ticket)
-                    self.particles[key] = p
-                    if c in self.cells:
-                        self.cells[c] + key
-                    else:
-                        self.cells[c] = CellWithParticles(self, c, key)
-                    key += 1
-                else:
-                    not_found.append(p)
+            not_found = self.__add_particles_local(received)
             count = len(not_found)
             count_global = self.comm.allgather(count)
-
-            # if self.comm.rank == 0: info('%s' % count_global)
+            # info('%d %s' % (loop, count_global))
         self.comm.barrier()
-   #  def add_particles(self, particles):
-   #      for i, particle in enumerate(list_of_particles):
-   #          c = self.locate(particle)
-   #          if not (c == -1 or c == __UINT32_MAX__):
-   #              my_found[i] = True
-   #              if not has_properties:
-   #                  pmap += self.mesh, c, particle
-   #              else:
-   #                  # Get values of properties for this particle
-   #                  for key in properties:
-   #                      particle_properties[key] = properties_d[key][i]
-   #                  pmap += self.mesh, c, particle, particle_properties
-   #      # All particles must be found on some process
-   #      comm.Reduce(my_found, all_found, root=0)
 
-   #      if self.myrank == 0:
-   #          missing = np.where(all_found == 0)[0]
-   #          n_missing = len(missing)
+        return not_found
 
-   #          assert n_missing == 0,\
-   #              '%d particles are not located in mesh' % n_missing
 
-   #          # Print particle info
-   #          if self.__debug:
-   #              for i in missing:
-   #                  print 'Missing', list_of_particles[i].position
+    def step(self, u, dt):
+        'Move particles by forward Euler x += u*dt'
+        for cwp in self.cells.itervalues():
+            vertex_coordinates, orientation = cwp.get_vertex_coordinates(), cwp.orientation()
+            # Restrict once per cell
+            u.restrict(self.coefficients,
+                       self.element,
+                       cwp,
+                       vertex_coordinates,
+                       cwp)
+            for particle in cwp.particles:
+                x = self.particles[particle][:self.dim]
+                # Compute velocity at position x
+                self.element.evaluate_basis_all(self.basis_matrix,
+                                                x,
+                                                vertex_coordinates, 
+                                                orientation)
+                x[:] = x[:] + dt*np.dot(self.coefficients, self.basis_matrix)[:]
+        self.relocate()
 
-   #              n_duplicit = len(np.where(all_found > 1)[0])
-   #              print 'There are %d duplicit particles' % n_duplicit
+
+    def relocate(self):
+        cell_map = defaultdict(list)
+        empty_cells = []
+        for cwp in self.cells.itervalues():
+
+            left = []
+            for particle in cwp.particles:
+                x = self.particles[particle][:self.dim]
+
+                point = df.Point(*x)
+                # Search only if particle moved outside original cell
+                if not cwp.contains(point):
+                    left.append(particle)
+                    # FIXME
+                    found = False
+                    # Check neighbor cells
+                    for neighbor in cwp.neighbors:
+                        if neighbor.contains(point):
+                            new_cell = neighbor.index()
+                            found = True
+                            break
+                    # Do a completely new search if not found by now
+                    if not found: new_cell = self.locate(x)
+                    # Record to map
+                    cell_map[new_cell].append(particle)
+            cwp.particles.difference_update(set(left))
+            if not cwp.particles: empty_cells.append(cwp.index())
+        for cell in empty_cells: self.cells.pop(cell)
+
+        # Add locally found particles
+        local_cells = cell_map.keys()
+        if -1 in local_cells:
+            local_cells.remove(-1)
+            non_local = cell_map[-1]
+        else:
+            non_local = []
+
+        for c in local_cells:
+            if c not in self.cells:
+                self.cells[c] = CellWithParticles(self, c)
+            for p in cell_map[c]: self.cells[c] + p
+
+        particles = [self.particles[i] for i in non_local]
+        count_global = self.comm.allgather(len(particles))
+        not_found = self.__add_particles_global(count_global, particles)
+        for i in non_local: self.particles.pop(i)
+
+        lost = self.comm.allreduce(len(not_found))
+        # info('%d' % lost)
 
 # ----------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    from dolfin import UnitSquareMesh, VectorFunctionSpace, info, Timer
+    from dolfin import UnitSquareMesh, VectorFunctionSpace, info, Timer, interpolate
+    from dolfin import Expression
     import sys
 
-    mesh = UnitSquareMesh(2, 2)
+    mesh = UnitSquareMesh(20, 20)
     V = VectorFunctionSpace(mesh, 'CG', 1)
+    v = interpolate(Expression(('-(x[1]-0.5)', 'x[0]-0.5'), degree=1), V)
 
     property_layout = {}#{'q': 1}
     lpc = LPCollection(V, property_layout)
@@ -174,11 +221,14 @@ if __name__ == '__main__':
         particles = np.c_[particles_x, particles_q]
         lpc.add_particles(particles_x)
     dt = t.stop()
-    info('%g' % dt)
+    # info('%g' % dt)
 
-    # if lpc.comm.rank == 0:
-    #     print lpc.particles
-    #     for i, c in lpc.cells.iteritems():
-    #         print i, c.particles
+    #print lpc.particles
+    for i in range(20):
+        print i
+        lpc.step(v, 0.001)
 
-    
+    # print lpc.particles
+    # lpc.step(v, 0.01)
+
+    # info('%s' % lpc.particles.keys())
